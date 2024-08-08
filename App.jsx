325ca3bdd2747d4b881f4515a59962bd97706fd9
zeeshan__kharadi <162435122+zeeshan91kharadi@users.noreply.github.com>
2024-08-08 16:33:15 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import BACKGROUND from './components/background'
+import FOREGROUND from './components/foreground'
+
+
+
+function App() {
+  return (
+<div className=' w-full h-screen bg-zinc-800'>
+   <BACKGROUND />
+<FOREGROUND />
+  </div>
+  )
+}
+
+export default App
---
+++ BACKGROUND.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+function background() {
+  return (
+    <>
+        
+<div className='fixed z-[2] w-full h-screen '>
+      <div className='absolute top-[5%] w-full py-10   flex justify-center text-zinc-600 font-semibold text-xl'>docs document</div>
+
+   <h1 className=' absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vh] leading-none taracking-tighter font-semibold text-zinc-900'>DOCS.</h1>
+    </div>
+    </>
+  )
+}
+
+export default background
